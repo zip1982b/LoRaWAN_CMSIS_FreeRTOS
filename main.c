d1ad01fca849e64a994bc441fadd1ce1cf27a31d
zip1982b <zip1982b@gmail.com>
2020-07-17 13:47:12 +0200

    add feature UART and Ring fifo

--- main.c
+++ main.c
@@ -3,18 +3,33 @@
 #include "FreeRTOS.h"
 #include "task.h"
 #include "queue.h"
+#include "uart.h"
+
 
 
 void vMCU_worked(void *arg);
+void vLoRaWAN_Modem(void *arg);
 
 
+static const UARTInitStructure_t UARTInitStr = 
+{
+  .bus_freq = 36000000,
+  .baud = 9600,
+  .data_bits = 8,
+  .stop_bits = 1,
+  .parity = 0,
+};
 
 int main()
 {
   if(!ClockInit()){
     PortInit();
-    xTaskCreate(vMCU_worked, "MCU worked", configMINIMAL_STACK_SIZE, NULL, 1, NULL);
+    UART_Init(2, &UARTInitStr);
     
+    
+    
+    xTaskCreate(vMCU_worked, "MCU worked", configMINIMAL_STACK_SIZE, NULL, 1, NULL);
+    xTaskCreate(vLoRaWAN_Modem, "RHF78-052", 256, NULL, 2, NULL);
     vTaskStartScheduler();
   }
   else return 0;
@@ -30,9 +45,19 @@ void vMCU_worked(void *arg){
   
   while(1){
     PortSetHi();
-    vTaskDelay(3000 / portTICK_RATE_MS);
+    vTaskDelay(1000 / portTICK_RATE_MS);
     PortSetLow();
-    vTaskDelay(3000 / portTICK_RATE_MS);
+    vTaskDelay(1000 / portTICK_RATE_MS);
+  }
+}
+
+
+void vLoRaWAN_Modem(void *arg){
+  UART_ReadBuffClear(2);
+  UART_WriteBuffClear(2);
+  
+  while(1){
+    
   }
 }
 
